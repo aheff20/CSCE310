87cf8b83c60fc13432c7ff4314bd65e8628cf635
lucaswilber <112518612+lucaswilber@users.noreply.github.com>
2023-12-01 09:14:35 -0600

    Update NavBar to give Users access to the Programs and Events routes

--- client/src/components/Programs/programs.js
+++ client/src/components/Programs/programs.js
@@ -10,32 +10,38 @@ import React from "react";
 function Programs(props) {
   const history = useHistory();
   const [loading, setLoading] = useState(true);
-  
+  const [allUserData, setAllUserData] = useState([]);
+
 
   useEffect(() => {
     setLoading(false);
-    
+    axios
+      .get("/users/getAllUserData")
+      .then((res) => {
+        setAllUserData(res.data);
+        setLoading(false);
+      })
 
-  }, []);  
+  }, []);
 
   return (
-    <div className="Events">
+    <div className="Programs">
       <Container>
         {loading ? (
-            <center>
-                {" "}
-                <Spinner animation="border" />{" "}
-            </center>
-        ):(
-            <React.Fragment>
-                <br></br>
-                Welcome to programs page bub.
+          <center>
+            {" "}
+            <Spinner animation="border" />{" "}
+          </center>
+        ) : (
+          <React.Fragment>
+            <br></br>
+            Welcome to programs page bub.
 
-            </React.Fragment>
+          </React.Fragment>
         )}
-        </Container>
-        </div>
-      
+      </Container>
+    </div>
+
   );
 }
 
