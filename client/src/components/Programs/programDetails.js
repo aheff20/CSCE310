db55e78c31514ff61c8964b04893d322d9864ec8
lacarbo <lacarbo@tamu.edu>
2023-12-03 16:20:39 -0600

    program Details admin format set up (no routing yet)

--- client/src/components/Programs/programDetails.js
+++ client/src/components/Programs/programDetails.js
@@ -13,6 +13,7 @@ function ProgramDetails(props) {
   const [loading, setLoading] = useState(true);
   const [allUserData, setAllUserData] = useState([]);
   const [programInfo, setProgramInfo] = useState({});
+  /* program_description, program_name, program_num [can ignore] */
 
   const programNum = useParams().programNum;
 
@@ -24,14 +25,48 @@ function ProgramDetails(props) {
         }})
         .then((res) => {
             setProgramInfo(res.data);
+            // get additional info only if admin
+            if (props.auth.user.user_type == "Admin") {
+              axios
+                .get("/users/getAllUserData")
+                .then((res) => {
+                    setAllUserData(res.data);
+                    setLoading(false);
+                })
+            }
+            else setLoading(false);
         })
-    
-
-    
-
-    setLoading(false);
-    
   }, []);
+
+  const studentHandler = (userUIN) => {
+    
+  }
+
+  const getTable = () => {
+    const list = [];
+    for(let k = 0; k < allUserData.length; k++) {
+        
+        if(allUserData[k].user_type == "Admin") {
+            continue;
+        }
+        
+
+        const temp = <tr key={k}>
+                        <td>{allUserData[k].uin}</td>
+                        <td>{allUserData[k].first_name} {allUserData[k].m_initial}. {allUserData[k].last_name}</td>
+                        <td>{allUserData[k].email}</td>
+                        <td>{allUserData[k].username}</td>
+                        <td>{allUserData[k].discord}</td>
+                        <td>
+                            <Button variant="success btn-sm" onClick={() => studentHandler(allUserData[k].uin)}>
+                                Edit
+                            </Button>
+                        </td>
+                      </tr>
+        list.push(temp);
+    }
+    return list;
+  }
 
   return (
     <div className="Programs">
@@ -44,13 +79,31 @@ function ProgramDetails(props) {
         ) : (
           <React.Fragment>
             <br></br>
-            <h2 className="display-5 text-center">Program {programNum} Details</h2>
+            <h2 className="display-5 text-center">{programInfo.program_name}</h2>
+            <p className="text-center">{programInfo.program_description}</p>
 
             {props.auth.user.user_type == "Admin" ? (
                 // Return a table with all students information who are in this program (programNum)
                 // Each row in the table should be able to view more details in a modal to see more details/edit students
-                <></>
-
+                <React.Fragment>
+                  <div className="Admin Table">
+                      <h2 className="display-5 text-center">Enrolled Students</h2>
+                          <Table striped bordered hover className="text-center">
+                              <thead>
+                                  <tr>
+                                      <th className="col-md-1">UIN</th>
+                                      <th className="col-md-2">Name</th>
+                                      <th className="col-md-2">Email</th>
+                                      <th className="col-md-2">Username</th>
+                                      <th className="col-md-2">Discord</th>
+                                      <th className="col-md-1">Edit?</th>
+                                  </tr>
+                              </thead>
+                              <tbody>{getTable()}</tbody>
+                          </Table>
+                          <br></br>
+                  </div>
+                </React.Fragment>
             ) : (
                 // If the user is a student, they should have buttons to add new certifiates/classes/internships to the program
                 // Allow the user to see their progress in the program
